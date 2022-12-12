396deec781f7bc7f33adabc5c31fdf02115cee2e
Sumanthkovuri <119628426+Sumanthkovuri@users.noreply.github.com>
2022-12-11 21:04:38 -0600

    Update Main.java
    created accept method to accept the details from the user.

--- Main.java
+++ Main.java
@@ -133,7 +133,46 @@ public final void searchbygmail(String gm)
     // {
 
     // }
+public final void accept()
+    {
+        String number = new String(new char[50]);
+        String gmail = new String(new char[40]);
+        String name = new String(new char[30]);
+        char ans;
+			  /*do
+			  {*/
+        System.out.print("ENTER NAME      :");
+        name = sc.next();
+        // cin.getline (name,30);
+        System.out.print("ENTER NUMBER    :");
+        number = sc.next();
+        while (number.length() != 10)
+        {
+            System.out.print("ENTER VALID NUMBER  :");
+            number = sc.next();
+        }
+        System.out.print("ENTER G-MAIL    :");
+        gmail = sc.next();
+        temp = new dnode(name, number, gmail);
+        if (head == null)
+        {
+            head = temp;
+        }
+        else
+        {
+            ptr = head;
+            while (ptr.next != null)
+            {
+                ptr = ptr.next;
+            }
+            ptr.next = temp;
+            temp.prev = ptr;
+        }
+				/*System.out.print("DO YOU WANT TO CONTINUE?????????");
+			ans = sc.next().charAt(0);
+			  }while (ans == 'y');*/
 
+    }
     public final void display()
     {
         ptr = head; //start the node
